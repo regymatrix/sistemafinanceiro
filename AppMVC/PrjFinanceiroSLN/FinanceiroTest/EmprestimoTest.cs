6f41b7f04f0c7a069778cf4b96978b84d57325f4
regymatrix <105677816+regymatrix@users.noreply.github.com>
2026-03-21 11:45:16 -0300

    TDD emprestimo

---
+++ AppMVC/PrjFinanceiroSLN/FinanceiroTest/EmprestimoTest.cs
@@ -0,0 +1,27 @@
+
+using PrjFinanceiro.Services;
+
+namespace FinanceiroTest
+{
+    public class EmprestimoTest
+    {
+           
+
+        [Fact]
+        public void CalculoJurosSimples_Retorna1100()
+        {
+            //arrange
+            decimal valorEmprestado = 1000;
+            double taxa = 0.10;
+            decimal valorEsperado = 100;
+
+            EmprestimoService emprestimo= new EmprestimoService();
+            decimal resultado = emprestimo.CalcularJurosSimples(valorEmprestado, taxa);
+
+            Assert.Equal(valorEsperado, resultado);
+       
+
+
+        }
+    }
+}
---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Services/IEmprestimoService.cs
@@ -0,0 +1,8 @@
+﻿namespace PrjFinanceiro.Services
+{
+    public interface IEmprestimoService
+    {
+        decimal CalcularJurosSimples(decimal valor,  double taxa);
+
+    }
+}
