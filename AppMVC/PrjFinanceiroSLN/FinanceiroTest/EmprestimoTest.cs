a9cd579854a8527fca453af6001b999e0fd332be
LaraFabianD <lara.f.dantas240@gmail.com>
2026-03-21 11:48:07 -0300

    commit

---
+++ AppMVC/PrjFinanceiroSLN/FinanceiroTest/EmprestimoTest.cs
@@ -0,0 +1,22 @@
+using PrjFinanceiro.Services;
+
+namespace FinanceiroTest
+{
+    public class EmprestimoTest
+    {
+        [Fact]
+        public void CalculoJurosSimples_Retorna1100()
+        {
+            //arrange
+            decimal valoremprestado = 1000;
+            double taxa = 0.10;
+            decimal valorEsperado = 100;
+
+            EmprestimoService emprestimo  = new EmprestimoService();
+            decimal resultado = emprestimo.CalcularJurosSimples(valoremprestado, taxa);
+
+            Assert.Equal(valorEsperado, resultado);
+
+        }
+    }
+}
---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/ContaBancaria.cs
@@ -0,0 +1,12 @@
+﻿namespace PrjFinanceiro.Models
+{
+    public class ContaBancaria
+    {
+        public string NumeroConta { get; set; }
+        public decimal Saldo { get; set; }
+        public Cliente Cliente { get; set; }
+        public Agencia Agencia { get; set; }
+
+
+    }
+}
