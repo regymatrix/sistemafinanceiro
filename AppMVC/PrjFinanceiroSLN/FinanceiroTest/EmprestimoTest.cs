a52531af43bc23908ecde53b69e8fadc68619b06
be4frizz <biatansaslk@gmail.com>
2026-03-21 11:45:50 -0300

    TDD Emprestimo

---
+++ AppMVC/PrjFinanceiroSLN/FinanceiroTest/EmprestimoTest.cs
@@ -0,0 +1,21 @@
+using PrjFinanceiro.Services;
+
+namespace FinanceiroTest
+{
+    public class EmprestimoTest
+    {
+        [Fact]
+        public void CalculoJurosSimples_Retornar1100()
+        {
+            //arrange
+            decimal valorEmprestado = 1000;
+            double taxa = 0.10;
+            decimal valorEsperado = 100;
+
+            EmprestimoService emprestimo = new EmprestimoService();
+            decimal resultado = emprestimo.CalcularJurosSimples(valorEmprestado, taxa);
+
+            Assert.Equal(valorEsperado, resultado);
+        }
+    }
+}
---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Services/EmprestimoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrjFinanceiro.Services
+{
+    public class EmprestimoService : IEmprestimoService
+    {
+        public decimal CalcularJurosSimples(decimal valor, double taxa)
+        {
+            return (valor * (decimal)taxa);
+            /*throw new System.NotImplementedException();*/
+        }
+    }
+}
