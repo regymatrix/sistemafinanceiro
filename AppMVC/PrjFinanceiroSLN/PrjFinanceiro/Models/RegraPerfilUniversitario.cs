a5711bb9e43f6a60d6a8cffbe990b52aecf3e2ad
LannaLorrane <149171221+LannaLorrane@users.noreply.github.com>
2026-03-30 10:30:32 -0300

    Lilian-Score-R14 (3)

--- AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/RegraPerfilUniversitario.cs
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/RegraPerfilUniversitario.cs
@@ -1,6 +1,19 @@
-﻿namespace PrjFinanceiro.Models
+﻿using System.Runtime.InteropServices;
+
+namespace PrjFinanceiro.Models
 {
-    public class PerfilUniversitario
+    public class RegraPerfilUniversitario : IRegraEscore
     {
+        public int CalcularPontuacao(Cliente cliente, ContaBancaria conta)
+        {
+            if (cliente is null || string.IsNullOrWhiteSpace(cliente.Nome)) return 0;
+
+            if (cliente.Nome.ToUpper().Contains("EIRELI") || cliente.Nome.ToUpper().Contains("LTDA"))
+            {
+                return -80;
+            }
+
+            return 0;
+        }
     }
 }
