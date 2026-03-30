faa65911b4ac54bfd2d8be71006f3b0d261d4c1c
LaraFabianD <lara.f.dantas240@gmail.com>
2026-03-30 09:46:04 -0300

    commit_Lara_R13

---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/RegraSaldoSeguranca.cs
@@ -0,0 +1,16 @@
+﻿namespace PrjFinanceiro.Models
+{
+    public class RegraSaldoSeguranca : IRegraEscore
+    {
+        public int CalcularPontuacao(Cliente cliente, ContaBancaria conta)
+        {
+            if (conta is null) return 0;
+
+            if (conta.Saldo > 50000)
+            {
+                return 150;
+            }
+            return 0; 
+        }
+    }
+}
