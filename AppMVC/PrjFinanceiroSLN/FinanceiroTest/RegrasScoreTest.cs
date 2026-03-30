61cad3342f6992947298de96fc2b216d8ce2a8f8
be4frizz <biatansaslk@gmail.com>
2026-03-30 11:14:30 -0300

    beatriz score R16

--- AppMVC/PrjFinanceiroSLN/FinanceiroTest/RegrasScoreTest.cs
+++ AppMVC/PrjFinanceiroSLN/FinanceiroTest/RegrasScoreTest.cs
@@ -147,26 +147,15 @@ namespace FinanceiroTest
 
             Assert.Equal(0, resultado);
         }
-        public void RegraSaldoSeguranca_Clientecom50000_DeveRetornar_150()
+        [Fact]
+        public void RegraIncentivoCentroOeste_DF_Retorna60()
         {
-            
-            var regra = new RegraSaldoSeguranca();
-            var cliente = new Cliente { Nome = "Teste" };
-            var conta = new ContaBancaria { Saldo = 50000m }; 
+            var regra = new RegraIncentivoCentroOeste();
+            var cliente = new Cliente { EstadoUF = "DF" };
 
-            int resultado = regra.CalcularPontuacao(cliente, conta);
-            Assert.Equal(+150, resultado);
+            int resultado = regra.CalcularPontuacao(cliente, null);
+
+            Assert.Equal(60, resultado);
         }
-        public void RegraContaNegativa_ClienteNegativo_DeveRetornar_Menos200()
-        {
-
-            var regra = new RegraContaNegativa();
-            var cliente = new Cliente { Nome = "Teste" };
-            var conta = new ContaBancaria { Saldo = 0m };
-
-            int resultado = regra.CalcularPontuacao(cliente, conta);
-            Assert.Equal(+200, resultado);
-        }
-
     }
 }
---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/RegraIncentivoCentroOeste.cs
@@ -0,0 +1,13 @@
+﻿namespace PrjFinanceiro.Models
+{
+    public class RegraIncentivoCentroOeste : IRegraEscore
+    {
+        public int CalcularPontuacao(Cliente cliente, ContaBancaria conta)
+        {
+            if (cliente.EstadoUF == "DF"|| cliente.EstadoUF == "GO" || cliente.EstadoUF == "MT")
+                return 60;
+
+            return 0;
+        }
+    }
+}
