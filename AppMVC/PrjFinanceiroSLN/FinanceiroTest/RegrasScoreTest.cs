f3d7dbc0e08098adeeecb331a1ba02f03dda1346
Elias Dev <elias.m.dev@gmail.com>
2026-03-30 09:37:25 -0300

    RegraSaldoDeSeguranca

--- AppMVC/PrjFinanceiroSLN/FinanceiroTest/RegrasScoreTest.cs
+++ AppMVC/PrjFinanceiroSLN/FinanceiroTest/RegrasScoreTest.cs
@@ -147,5 +147,40 @@ namespace FinanceiroTest
 
             Assert.Equal(0, resultado);
         }
+
+        [Fact]
+        public void RegraSaldoDeSeguranca_SaldoMaior50000_DeveRetornar_150Positivo()
+        {
+            var regra = new RegraSaldoDeSeguranca();
+            var cliente = new Cliente { Nome = "Joao"};
+            var contabancaria = new ContaBancaria { Saldo = 50001m };
+            int resultado = regra.CalcularPontuacao(cliente, contabancaria);
+
+            Assert.Equal(+150, resultado);
+        }
+
+        [Fact]
+        public void RegraSaldoDeSeguranca_ClienteNulo_DeveRetornar_0()
+        {
+            var regra = new RegraSaldoDeSeguranca();
+            var contabancaria = new ContaBancaria { Saldo = 50001m };
+            int resultado = regra.CalcularPontuacao(null, contabancaria);
+
+            Assert.Equal(0, resultado);
+        }
+
+        public void RegraSaldoDeSeguranca_SaldoNulo_DeveRetornar_0()
+        {
+            var regra = new RegraSaldoDeSeguranca();
+            var cliente = new Cliente { Nome = "Joao" };
+            int resultado = regra.CalcularPontuacao(cliente, null);
+            Assert.Equal(0, resultado);
+        }
+        public void RegraSaldoDeSeguranca_ClienteeSaldoNulo_DeveRetornar_0()
+        {
+            var regra = new RegraSaldoDeSeguranca();
+            int resultado = regra.CalcularPontuacao(null, null);
+            Assert.Equal(0, resultado);
+        }
     }
 }
