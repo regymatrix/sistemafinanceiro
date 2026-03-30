727f264a8678fbc792a72b9eef1c061af4e535e3
LannaLorrane <149171221+LannaLorrane@users.noreply.github.com>
2026-03-30 10:41:29 -0300

    Lilian-Score-R15

--- AppMVC/PrjFinanceiroSLN/FinanceiroTest/RegrasScoreTest.cs
+++ AppMVC/PrjFinanceiroSLN/FinanceiroTest/RegrasScoreTest.cs
@@ -138,7 +138,41 @@ namespace FinanceiroTest
         [Fact]
         public void RegraPerfilUniversitario_NomeComLTDA_RetornaMenos80()
         {
-            var regra = new RegraSaldoSeguranca();
+            var regra = new RegraPerfilUniversitario();
+            var cliente = new Cliente { Nome = "Empresa LTDA" };
+
+            int resultado = regra.CalcularPontuacao(cliente, null);
+
+            Assert.Equal(-80, resultado);
+        }
+
+        [Fact]
+        public void RegraPerfilUniversitario_NomeComEIRELI_RetornaMenos80()
+        {
+            var regra = new RegraPerfilUniversitario();
+            var cliente = new Cliente { Nome = "Mercado EIRELI" };
+
+            int resultado = regra.CalcularPontuacao(cliente, null);
+
+            Assert.Equal(-80, resultado);
+        }
+
+        [Fact]
+        public void RegraPerfilUniversitario_NomePessoaFisica_Retorna0()
+        {
+            var regra = new RegraPerfilUniversitario();
+            var cliente = new Cliente { Nome = "Lili" };
+
+            int resultado = regra.CalcularPontuacao(cliente, null);
+
+            Assert.Equal(0, resultado);
+        }
+
+        [Fact]
+        public void RegraPerfilUniversitario_ClienteNulo_Retorna0()
+        {
+            var regra = new RegraPerfilUniversitario();
+            var cliente = new Cliente { Nome = "EmpresaLTDA" };
 
             int resultado = regra.CalcularPontuacao(null, null);
 
