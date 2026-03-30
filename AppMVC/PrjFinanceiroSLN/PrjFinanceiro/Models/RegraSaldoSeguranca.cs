698f833f28178f532c5d6f00aa17de55927c5a39
LannaLorrane <149171221+LannaLorrane@users.noreply.github.com>
2026-03-30 09:49:38 -0300

    Lilian-Score-R13 (2)

--- AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/RegraSaldoSeguranca.cs
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/RegraSaldoSeguranca.cs
@@ -1,6 +1,21 @@
-﻿namespace PrjFinanceiro.Models
+﻿using System.Collections.Generic;
+
+namespace PrjFinanceiro.Models
 {
-    public class RegraSaldoDeSeguranca
+    public class RegraSaldoSeguranca : IRegraEscore
     {
+        
+            public int CalcularPontuacao(Cliente cliente, ContaBancaria conta)
+            {
+                if (conta is null) return 0;
+
+                if (conta.Saldo>50000)
+                {
+                    return 150;
+                }
+
+                return 0;
+            }
+        
     }
 }
