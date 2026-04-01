f4417a29dcb557b76b86238b99acf31c0bf0d966
be4frizz <biatansaslk@gmail.com>
2026-04-01 11:06:37 -0300

    beatriz score R22

---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/RegraDeEstabilidade.cs
@@ -0,0 +1,15 @@
+﻿namespace PrjFinanceiro.Models
+{
+    public class RegraDeEstabilidade : IRegraEscore
+    {
+        public int CalcularPontuacao(Cliente cliente, ContaBancaria conta)
+        {
+            if (cliente.EstadoUF == "MG" || cliente.EstadoUF == "MG")
+            {
+                return 25;
+            }
+
+            return 0;
+        }
+    }
+}
