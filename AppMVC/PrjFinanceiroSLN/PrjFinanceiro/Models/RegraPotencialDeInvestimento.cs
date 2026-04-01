fa31a72e61e80ecf2a4e2d7b84ff0e5941b729a6
be4frizz <biatansaslk@gmail.com>
2026-04-01 11:00:16 -0300

    beatriz score R21

---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/RegraPotencialDeInvestimento.cs
@@ -0,0 +1,15 @@
+﻿namespace PrjFinanceiro.Models
+{
+    public class RegraPotencialDeInvestimento : IRegraEscore
+    {
+        public int CalcularPontuacao(Cliente cliente, ContaBancaria conta)
+        {
+            if (conta.Saldo > 2000m)
+            {
+                return 45;
+            }
+
+            return 0;
+        }
+    }
+}
