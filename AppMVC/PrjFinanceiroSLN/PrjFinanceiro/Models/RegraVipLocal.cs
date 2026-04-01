858b080f7a2231382647b75043f35e01f47a8369
LannaLorrane <lanna.lorrane18@gmail.com>
2026-04-01 09:24:41 -0300

    Commit-Lanna-R18

---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/RegraVipLocal.cs
@@ -0,0 +1,10 @@
+﻿namespace PrjFinanceiro.Models
+{
+    public class RegraVipLocal : IRegraEscore
+    {
+        public int CalcularPontuacao(Cliente cliente, ContaBancaria conta) =>
+        cliente?.EstadoUF == "SE" ? 120 : 0;
+
+
+    }
+}
