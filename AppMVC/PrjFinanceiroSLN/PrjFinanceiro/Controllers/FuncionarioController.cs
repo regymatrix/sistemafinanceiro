2e2f970175fab23e26961216ce8a0d47b54f5a6f
dayvisson2122 <166879693+dayvisson2122@users.noreply.github.com>
2026-04-18 11:39:08 -0300

    funcionario crud 1

---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Controllers/FuncionarioController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrjFinanceiro.Models;
+using System;
+using System.Linq;
+
+namespace PrjFinanceiro.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public FuncionarioController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var lista = _context.Funcionario.ToList();
+            ViewBag.nomesenai = "SENAI";
+
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(string nome, DateTime datanascimento, string cidade, string estadoUF, string cpf, string telefone)
+        {
+            var novoFuncionario = new Funcionario
+            {
+                Nome = nome,
+                DataNascimento = datanascimento,
+                Cidade = cidade,
+                EstadoUF = estadoUF,
+                CPF = cpf,
+                Telefone = telefone
+            };
+
+            if (!string.IsNullOrEmpty(nome))
+            {
+                _context.Funcionario.Add(novoFuncionario);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var funcionario = _context.Funcionario.FirstOrDefault(f => f.Codigo == id);
+
+            if (funcionario == null)
+            {
+                return NotFound();
+            }
+
+            return View(funcionario);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(int codigo, string nome, DateTime datanascimento, string cidade, string estadoUF, string cpf, string telefone)
+        {
+            var funcionarioNoBanco = _context.Funcionario.FirstOrDefault(f => f.Codigo == codigo);
+
+            if (funcionarioNoBanco != null)
+            {
+                funcionarioNoBanco.Nome = nome;
+                funcionarioNoBanco.DataNascimento = datanascimento;
+                funcionarioNoBanco.Cidade = cidade;
+                funcionarioNoBanco.EstadoUF = estadoUF;
+                funcionarioNoBanco.CPF = cpf;
+                funcionarioNoBanco.Telefone = telefone;
+
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            var funcionario = _context.Funcionario.FirstOrDefault(f => f.Codigo == id);
+
+            if (funcionario == null)
+            {
+                return NotFound();
+            }
+
+            return View(funcionario);
+        }
+
+        [HttpPost]
+        public IActionResult ExcluirConfirmado(int codigo)
+        {
+            var funcionario = _context.Funcionario.FirstOrDefault(f => f.Codigo == codigo);
+
+            if (funcionario != null)
+            {
+                _context.Funcionario.Remove(funcionario);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/AppDbContext.cs
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/AppDbContext.cs
@@ -9,6 +9,7 @@ namespace PrjFinanceiro.Models
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Agencia> Agencia { get; set; }
+        public DbSet<Funcionario> Funcionario { get; set; }
 
     }
 }
