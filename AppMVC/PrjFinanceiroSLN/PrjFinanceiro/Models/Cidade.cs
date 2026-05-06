d3a25ed717a15f15588c3596833bd5c9f0ead2c7
RobertadeFigueredo <149171221+LannaLorrane@users.noreply.github.com>
2026-05-06 10:50:06 -0300

    Commit01- Cidade

---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/Cidade.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrjFinanceiro.Models
+{
+    public class Cidade
+    {
+
+        [Key]
+        public int Codigo { get; set; }
+        public string NomeCidade { get; set; }
+        public string CodigoIBGE { get; set; }
+        public int CodigoEstado { get; set; }
+    }
+}
