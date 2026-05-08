584522141b4015575edd578b8943704cc15101bf
Aryelle9 <lara.f.dantas240@gmail.com>
2026-05-08 15:32:40 -0300

    commitBairroLara

---
+++ DataBase/04_dbScript_TBBairro.sql
@@ -0,0 +1,16 @@
+
+use dbFinanceiro
+go
+
+Create table BAIRRO (
+
+Codigo int identity(1,1) primary key,
+NomeBairro varchar(100) not null,
+CodigoCidade int not null,
+CONSTRAINT fk_Codigo_Cidade
+FOREIGN KEY (CodigoCidade) REFERENCES Cidade(Codigo) 
+)
+
+--INSERT Estado VALUES('Sergipe','SE')
+
+SELECT * FROM BAIRRO
