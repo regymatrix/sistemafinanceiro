77ae2c70be0df222292fcd6bc48d7018fd87a683
maria <guilherme.l.dev@gmail.com>
2026-05-11 10:02:21 -0300

    CRUD Escolaridade e Etnia implementado

---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Controllers/EscolaridadeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrjFinanceiro.Models;
+using System.Linq;
+
+namespace PrjFinanceiro.Controllers
+{
+    public class EscolaridadeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public EscolaridadeController(AppDbContext context) { _context = context; }
+
+        public IActionResult Index() => View(_context.Escolaridade.ToList());
+
+        [HttpGet] public IActionResult Criar() => View();
+
+        [HttpPost]
+        public IActionResult Criar(string Descricao)
+        {
+            _context.Escolaridade.Add(new Escolaridade { Descricao = Descricao });
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id) => View(_context.Escolaridade.Find(id));
+
+        [HttpPost]
+        public IActionResult Editar(int codigo, string Descricao)
+        {
+            var obj = _context.Escolaridade.Find(codigo);
+            if (obj != null)
+            {
+                obj.Descricao = Descricao;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            var registro = _context.Escolaridade.FirstOrDefault(e => e.Codigo == id);
+
+            if (registro == null) return NotFound();
+
+            return View(registro);
+        }
+
+        [HttpPost]
+        public IActionResult ExcluirConfirmado(int codigo)
+        {
+            var escolaridade = _context.Escolaridade.FirstOrDefault(e => e.Codigo == codigo);
+            if (escolaridade != null)
+            {
+                _context.Escolaridade.Remove(escolaridade);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ExcluirConfirmadoModal(int codigo)
+        {
+            var registro = _context.Escolaridade.Find(codigo); // Ou _context.Etnia
+            if (registro != null)
+            {
+                _context.Escolaridade.Remove(registro);
+                _context.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Controllers/EtniaController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrjFinanceiro.Models;
+using System.Linq;
+
+namespace PrjFinanceiro.Controllers
+{
+    public class EtniaController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EtniaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var lista = _context.Etnia.ToList();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Criar() => View();
+
+        [HttpPost]
+        public IActionResult Criar(string Descricao)
+        {
+            if (!string.IsNullOrEmpty(Descricao))
+            {
+                var novaEtnia = new Etnia { Descricao = Descricao };
+                _context.Etnia.Add(novaEtnia);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var etnia = _context.Etnia.Find(id);
+            if (etnia == null) return NotFound();
+            return View(etnia);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(int codigo, string Descricao)
+        {
+            var etnia = _context.Etnia.Find(codigo);
+            if (etnia != null)
+            {
+                etnia.Descricao = Descricao;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            var etnia = _context.Etnia.Find(id);
+            if (etnia == null) return NotFound();
+            return View(etnia);
+        }
+
+        [HttpPost]
+        public IActionResult ExcluirConfirmado(int codigo)
+        {
+            var etnia = _context.Etnia.Find(codigo);
+            if (etnia != null)
+            {
+                _context.Etnia.Remove(etnia);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ExcluirConfirmadoModal(int codigo)
+        {
+            var etnia = _context.Etnia.Find(codigo);
+            if (etnia != null)
+            {
+                _context.Etnia.Remove(etnia);
+                _context.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
