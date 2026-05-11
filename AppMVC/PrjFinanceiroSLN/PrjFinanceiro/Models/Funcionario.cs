687e326ccc07d48790a9e0b94221c4925403b8f1
dayvisson2122 <166879693+dayvisson2122@users.noreply.github.com>
2026-05-11 09:27:13 -0300

    funcionarioFK

--- AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/Funcionario.cs
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/Funcionario.cs
@@ -1,19 +1,27 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using Microsoft.IdentityModel.Tokens;
+﻿using PrjFinanceiro.Models;
+using System;
 
-namespace PrjFinanceiro.Models
+public class Funcionario
 {
-    public class Funcionario
-    {
-        [Key]
-        public int Codigo { get; set; }
-        public string Nome { get; set; }
-        public DateTime DataNascimento { get; set; }
-        public string Cidade { get; set; }
-        public string EstadoUF { get; set; }
-        public string CPF { get; set; }
-        public string Telefone { get; set; }
+    public int Codigo { get; set; }
 
-    }
+    public string Nome { get; set; }
+
+    public DateTime DataNascimento { get; set; }
+
+    public string Cidade { get; set; }
+
+    public string EstadoUF { get; set; }
+
+    public string CPF { get; set; }
+
+    public string Telefone { get; set; }
+
+
+    public int CodigoEscolaridade { get; set; }
+    public Escolaridade Escolaridade { get; set; }
+
+
+    public int CodigoEtnia { get; set; }
+    public Etnia Etnia { get; set; }
 }
