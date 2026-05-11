3548d252d1a3035ca3712c1d7f54a5c0c71d4f77
maria <maria.l.santos13@aluno.senai.br>
2026-05-11 09:28:26 -0300

    CRUD Cidade Bairro Implementado

---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Controllers/BairroController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrjFinanceiro.Models;
+using System.Linq;
+
+namespace PrjFinanceiro.Controllers
+{
+    public class BairroController : Controller
+    {
+        private readonly AppDbContext _context;
+        public BairroController(AppDbContext context) { _context = context; }
+
+        public IActionResult Index() => View(_context.Bairro.Include(b => b.Cidade).ToList());
+
+        [HttpGet]
+        public IActionResult Criar()
+        {
+            ViewBag.Cidades = _context.Cidade.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(string NomeBairro, int CodigoCidade)
+        {
+            var novo = new Bairro { NomeBairro = NomeBairro, CodigoCidade = CodigoCidade };
+            _context.Bairro.Add(novo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var bairro = _context.Bairro.Find(id);
+            if (bairro == null) return NotFound();
+            ViewBag.Cidades = _context.Cidade.ToList();
+            return View(bairro);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(int codigo, string NomeBairro, int CodigoCidade)
+        {
+            var b = _context.Bairro.Find(codigo);
+            if (b != null)
+            {
+                b.NomeBairro = NomeBairro;
+                b.CodigoCidade = CodigoCidade;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ExcluirConfirmadoModal(int codigo)
+        {
+            var b = _context.Bairro.Find(codigo);
+            if (b != null)
+            {
+                _context.Bairro.Remove(b);
+                _context.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Controllers/CidadeController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrjFinanceiro.Models;
+using System.Linq;
+
+namespace PrjFinanceiro.Controllers
+{
+    public class CidadeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CidadeController(AppDbContext context) { _context = context; }
+
+        public IActionResult Index()
+        {
+            // Carrega a cidade junto com o objeto Estado para exibir o nome na lista
+            var lista = _context.Cidade.Include(c => c.Estado).ToList();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Criar()
+        {
+            // Envia a lista de estados para o dropdown
+            ViewBag.Estados = _context.Estado.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(string NomeCidade, string CodigoIBGE, int CodigoEstado)
+        {
+            var novaCidade = new Cidade
+            {
+                NomeCidade = NomeCidade,
+                CodigoIBGE = CodigoIBGE,
+                CodigoEstado = CodigoEstado
+            };
+            _context.Cidade.Add(novaCidade);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var cidade = _context.Cidade.Find(id);
+            if (cidade == null) return NotFound();
+            ViewBag.Estados = _context.Estado.ToList();
+            return View(cidade);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(int codigo, string NomeCidade, string CodigoIBGE, int CodigoEstado)
+        {
+            var cid = _context.Cidade.Find(codigo);
+            if (cid != null)
+            {
+                cid.NomeCidade = NomeCidade;
+                cid.CodigoIBGE = CodigoIBGE;
+                cid.CodigoEstado = CodigoEstado;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ExcluirConfirmadoModal(int codigo)
+        {
+            var cid = _context.Cidade.Find(codigo);
+            if (cid != null)
+            {
+                _context.Cidade.Remove(cid);
+                _context.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
