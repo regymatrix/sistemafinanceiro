8a97779f1e6d94b00cbc84a59e3c16abdb9372b3
luizatvs <elias.m.dev@gmail.com>
2026-05-16 11:19:32 -0300

    Part 1 arrumada

--- AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Controllers/CidadeApiController.cs
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Controllers/CidadeApiController.cs
@@ -1,6 +1,31 @@
-﻿namespace PrjFinanceiro.Controllers
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PrjFinanceiro.DTOs;
+using PrjFinanceiro.Models;
+using System.Linq;
+
+namespace PrjFinanceiro.Controllers
 {
-    public class CidadeApiController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CidadeApiController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public CidadeApiController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public IActionResult ListarTodos()
+        {
+            var cidades = _context.Cidade.ToList();
+            return Ok(cidades);
+        
+        }
+
+
     }
 }
