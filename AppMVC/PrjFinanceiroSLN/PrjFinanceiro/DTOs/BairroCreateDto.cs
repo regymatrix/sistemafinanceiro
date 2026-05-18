41a6beadba5abb41fd66ba5469d8efdef8837346
luizatvs <elias.m.dev@gmail.com>
2026-05-18 08:47:50 -0300

    BAirroApi

---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/DTOs/BairroCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrjFinanceiro.DTOs
+{
+    public class BairroCreateDto
+    {
+        [Required(ErrorMessage = "O nome do bairro é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome do bairro não pode exceder 100 caracteres.")]
+        public string NomeBairro { get; set; }
+
+        [Required(ErrorMessage = "O código da cidade é obrigatório.")]
+        public int CodigoCidade { get; set; }
+    }
+}
