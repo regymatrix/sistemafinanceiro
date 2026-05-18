15689717a099d822b46d86313a4a2d5bd3ceffb4
Lilian <lilian.a.nunes@aluno.senai.br>
2026-05-18 09:32:19 -0300

    commit - 18/05 Api e Dto Funcionario

---
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/DTOs/FuncionarioCreateDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrjFinanceiro.DTOs
+{
+    public class FuncionarioCreateDto
+    {
+        [Required(ErrorMessage = "O nome do funcionário é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome não pode exceder 100 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
+        public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "A cidade é obrigatória.")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O estado/UF é obrigatório.")]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "A UF deve conter exatamente 2 caracteres.")]
+        public string EstadoUF { get; set; }
+
+        [Required(ErrorMessage = "O CPF é obrigatório.")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "O telefone é obrigatório.")]
+        public string Telefone { get; set; }
+    }
+}
