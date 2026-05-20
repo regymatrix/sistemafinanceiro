1ae3b2194024d5e1e697855f0545bb5af8b8b0d5
Lilian <lilian.a.nunes@aluno.senai.br>
2026-05-20 11:01:40 -0300

    Commit - 20/05 Cliente(completo)

--- AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/AppDbContext.cs
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/AppDbContext.cs
@@ -13,7 +13,9 @@ namespace PrjFinanceiro.Models
         public DbSet<Estado> Estado { get; set; }
         public DbSet<Cidade> Cidade { get; set; }
         public DbSet<Escolaridade> Escolaridade { get; set; }
-
+        public DbSet<Etnia> Etnia { get; set; }
+        public DbSet<Bairro> Bairro { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
 
 
     }
--- AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/Cliente.cs
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/Cliente.cs
@@ -1,11 +1,20 @@
-﻿namespace PrjFinanceiro.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PrjFinanceiro.Models
 {
     public class Cliente
     {
+        [Key]
         public int Codigo { get; set; }
-
         public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string TipoCliente { get; set; }
+        public string CPF { get; set; }
+        public string CNPJ { get; set; }
+        public int CodigoBairro { get; set; }
 
-        public string EstadoUF { get; set; }
     }
 }
+
